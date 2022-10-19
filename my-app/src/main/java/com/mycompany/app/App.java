4a195850f74a26a92b7869a6dac15e29dfe5ae08
Usman Asif <chusmanullah@gmail.com>
2022-10-19 14:09:08 -0400

    update

---
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,15 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Don't forget to be Awesome!");
+	System.out.println("this is another line");
+    }
+}
